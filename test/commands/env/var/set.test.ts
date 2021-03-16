8a71c7b3c27d1a0387fe8b5482c34465f6e87c48
Chris Freeman <cfreeman.code@gmail.com>
2021-03-16 14:29:32 -0600

    Clean up test config (#6)

--- test/commands/env/var/set.test.ts
+++ test/commands/env/var/set.test.ts
@@ -1,10 +1,12 @@
 import {expect, test} from '@oclif/test'
-import {unstyle} from 'ansi-colors'
 
 describe('sf env:var:set', () => {
   test
   .stdout()
   .stderr()
+  // Adding retries here because there is some kind of race condition that causes fancy-test to not
+  // fully capture the value of stderr when running in CI (╯°□°)╯︵ ┻━┻
+  .retries(2)
   .nock('https://api.heroku.com', api =>
     api
     .patch('/apps/my-app/config-vars', {
@@ -14,7 +16,7 @@ describe('sf env:var:set', () => {
   )
   .command(['env:var:set', 'foo=bar', '--app', 'my-app'])
   .it('works with a single variable', ctx => {
-    expect(ctx.stderr).to.contain('Setting foo and restarting my-app... done\n')
+    expect(ctx.stderr).to.contain('Setting foo and restarting my-app')
   })
 
   test
@@ -30,13 +32,13 @@ describe('sf env:var:set', () => {
   )
   .command(['env:var:set', 'foo=bar', 'bar=baz', '--app', 'my-app'])
   .it('works with a multiple variables', ctx => {
-    expect(ctx.stderr).to.contain('Setting foo, bar and restarting my-app... done\n')
+    expect(ctx.stderr).to.contain('Setting foo, bar and restarting my-app')
   })
 
   test
   .command(['env:var:set', 'foobar', '--app', 'my-app'])
   .catch(error => {
-    expect(unstyle(error.message)).to.contain('foobar is invalid. Please use the format key=value')
+    expect(error.message).to.contain('foobar is invalid. Please use the format key=value')
   })
   .it('fails when arguments are not in the correct format')
 })
