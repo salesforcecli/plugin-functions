ea3ee8437cdac8cd55a8e5fa804980ad04292eaa
Chris Freeman <cfreeman.code@gmail.com>
2021-03-16 14:16:25 -0600

    set TTY config in mocha init and remove cool hexagons

--- test/helpers/init.js
+++ test/helpers/init.js
@@ -1,6 +1,8 @@
-const chalk = require('chalk')
-chalk.enabled = false
+const {color} = require('@heroku-cli/color')
+color.enabled = false
 process.env.FORCE_COLOR = '0'
+
 process.stdout.columns = 80
 process.stderr.columns = 80
+
 process.env.CLI_UX_SKIP_TTY_CHECK = true
