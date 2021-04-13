4984e92bc54b9789b5e8563d6def2f17281a701a
Chris Freeman <cfreeman.code@gmail.com>
2021-04-13 16:32:18 -0600

    first pass on `env list`

---
+++ src/commands/env/list.ts
@@ -0,0 +1,233 @@
+import {flags} from '@oclif/command'
+import {Aliases, AuthInfo} from '@salesforce/core'
+import {identifyActiveOrgByStatus, OrgListUtil} from '@salesforce/plugin-org/lib/shared/orgListUtil'
+import {ExtendedAuthFields} from '@salesforce/plugin-org/lib/shared/orgTypes'
+import {cli} from 'cli-ux'
+import {sortBy} from 'lodash'
+import Command from '../../lib/base'
+import herokuColor from '@heroku-cli/color'
+import * as Heroku from '@heroku-cli/schema'
+import herokuVariant from '../../lib/heroku-variant'
+import {ComputeEnvironment, Dictionary} from '../../lib/sfdc-types'
+import roundToNearestMinutesWithOptions from 'date-fns/esm/fp/roundToNearestMinutesWithOptions/index.js'
+
+type EnvironmentType = 'org' | 'scratchorg' | 'compute'
+
+export default class EnvList extends Command {
+  static description = 'List all environments by type'
+
+  static examples = [
+    '$ sf env list',
+    '$ sf env list --all',
+    '$ sf env list --environment-type org --environment-type compute',
+  ]
+
+  static flags = {
+    all: flags.boolean({
+      description: 'show all available envs instead of scoping to active orgs and their connected compute envs',
+    }),
+    'environment-type': flags.string({
+      char: 't',
+      description: 'filter by one or more environment types (org, scratchorg, compute)',
+      options: ['org', 'scratchorg', 'compute'],
+      multiple: true,
+    }),
+  }
+
+  private aliases?: Aliases
+
+  private aliasReverseLookup?: Dictionary<string[]>
+
+  async resolveOrgs(all = false) {
+    let fileNames: Array<string> = []
+    try {
+      fileNames = await AuthInfo.listAllAuthFiles()
+    } catch (error) {
+      if (error.name === 'NoAuthInfoFound') {
+        this.error('No orgs Found')
+      } else {
+        throw error
+      }
+    }
+
+    const metaConfigs = await OrgListUtil.readLocallyValidatedMetaConfigsGroupedByOrgType(fileNames, {})
+
+    const groupedSortedOrgs = {
+      nonScratchOrgs: sortBy(metaConfigs.nonScratchOrgs, v => [v.alias, v.username]),
+      scratchOrgs: sortBy(metaConfigs.scratchOrgs, v => [v.alias, v.username]),
+      expiredScratchOrgs: metaConfigs.scratchOrgs.filter(org => !identifyActiveOrgByStatus(org)),
+    }
+
+    return {
+      nonScratchOrgs: groupedSortedOrgs.nonScratchOrgs,
+      scratchOrgs: all ?
+        groupedSortedOrgs.scratchOrgs :
+        groupedSortedOrgs.scratchOrgs.filter(identifyActiveOrgByStatus),
+    }
+  }
+
+  private async resolveEnvironments(orgs: Array<ExtendedAuthFields>) {
+    const account = await this.fetchAccount()
+
+    const {data: environments} = await this.client.get<Array<ComputeEnvironment>>(`/enterprise-accounts/${account.salesforce_org.owner.id}/apps`, {
+      headers: {
+        Accept: herokuVariant('evergreen'),
+      },
+    })
+
+    const environmentsWithAliases = await this.resolveAliasesForEnvironments(environments)
+
+    return this.resolveAliasesForConnectedOrg(environmentsWithAliases, orgs)
+  }
+
+  private async resolveAliasForValue(environmentName: string) {
+    if (!this.aliases) {
+      this.aliases = await Aliases.create({})
+    }
+
+    if (!this.aliasReverseLookup) {
+      const entries = this.aliases.entries()
+
+      this.aliasReverseLookup = entries.reduce((acc: Dictionary<string[]>, [alias, environmentName]) => {
+        if (typeof environmentName !== 'string') {
+          return acc
+        }
+
+        if (acc[environmentName]) {
+          acc[environmentName].push(alias)
+        } else {
+          acc[environmentName] = [alias]
+        }
+        return acc
+      }, {})
+    }
+
+    const match = this.aliasReverseLookup[environmentName]
+
+    if (match) {
+      return match[0]
+    }
+
+    return ''
+  }
+
+  private async resolveAliasesForEnvironments(envs: Array<ComputeEnvironment>) {
+    return Promise.all(envs.map(async env => {
+      return {
+        ...env,
+        alias: await this.resolveAliasForValue(env.name!),
+      }
+    }))
+  }
+
+  private resolveAliasesForConnectedOrg(envs: Array<ComputeEnvironment>, orgs: Array<ExtendedAuthFields>) {
+    return envs.map(env => {
+      const orgId = env.sales_org_connection?.sales_org_id
+
+      const orgAlias = orgs.reduce((result, org) => {
+        if (org.orgId === orgId) {
+          return org.alias ?? ''
+        }
+
+        return result
+      }, '')
+
+      return {
+        ...env,
+        orgAlias,
+      }
+    })
+  }
+
+  renderOrgTable(orgs: Array<ExtendedAuthFields>) {
+    cli.log(herokuColor.bold(`Type: ${herokuColor.cyan('Salesforce Org')}`))
+    cli.table(orgs, {
+      alias: {
+        header: 'ALIAS',
+        get: row => row.alias ?? '',
+      },
+      username: {
+        header: 'USERNAME',
+      },
+      orgId: {
+        header: 'ORG ID',
+      },
+      connectedStatus: {
+        header: 'STATUS',
+        get: row => herokuColor.green(row.connectedStatus ?? ''),
+      },
+    })
+  }
+
+  renderScratchOrgTable(orgs: Array<ExtendedAuthFields>) {
+    cli.log(herokuColor.bold(`Type: ${herokuColor.cyan('Scratch Org')}`))
+    cli.table(orgs, {
+      alias: {
+        header: 'ALIAS',
+        get: row => row.alias ?? '',
+      },
+      username: {
+        header: 'USERNAME',
+      },
+      orgId: {
+        header: 'ORG ID',
+      },
+      expirationDate: {
+        header: 'EXPIRATION',
+      },
+    })
+  }
+
+  renderComputeEnvironmentTable(envs: Array<ComputeEnvironment>) {
+    cli.log(herokuColor.bold(`Type: ${herokuColor.cyan('Compute Environment')}`))
+    cli.table(envs, {
+      alias: {
+        header: 'ALIAS',
+      },
+      sfdx_project_name: {
+        header: 'PROJECT NAME',
+      },
+      orgAlias: {
+        header: 'CONNECTED ORG ALIAS',
+      },
+      orgId: {
+        header: 'CONNECTED ORG ID',
+        get: row => row.sales_org_connection?.sales_org_id,
+      },
+      environmentName: {
+        header: 'COMPUTE ENVIRONMENT NAME',
+        get: row => row.name,
+      },
+    })
+  }
+
+  async run() {
+    const {flags} = this.parse(EnvList)
+    const project = await this.fetchSfdxProject()
+    const {nonScratchOrgs, scratchOrgs} = await this.resolveOrgs(flags.all)
+    const orgs = [...nonScratchOrgs, ...scratchOrgs]
+    const environments = await this.resolveEnvironments(orgs)
+
+    if (!flags.all) {
+      this.log(`Current environments for project ${project.name}\n`)
+    }
+
+    let types = flags['environment-type'] as Array<EnvironmentType>
+    if (!types) {
+      types = ['org', 'scratchorg', 'compute']
+    }
+
+    const tableLookup = {
+      org: () => this.renderOrgTable(nonScratchOrgs),
+      scratchorg: () => this.renderScratchOrgTable(scratchOrgs),
+      compute: () => this.renderComputeEnvironmentTable(environments),
+    }
+
+    types.forEach((type, idx) => {
+      tableLookup[type]()
+      if (idx < types.length - 1) {
+        cli.log('=====')
+      }
+    })
+  }
+}
