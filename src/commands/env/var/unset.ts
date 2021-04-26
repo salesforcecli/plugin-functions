7095555c491cc12c22624bbbde085605949eefa3
Zane Whitfield <zwhitfield@salesforce.com>
2021-04-26 15:20:55 -0700

    Update commands

--- src/commands/env/var/unset.ts
+++ src/commands/env/var/unset.ts
@@ -10,17 +10,20 @@ export default class ConfigUnset extends Command {
   static description = 'unset a single config value for an environment'
 
   static examples = [
-    '$ sf env:var:set foo --app=my-app',
+    '$ sf env var unset foo --environment=my-environment',
   ]
 
   static flags = {
-    app: flags.string({
+    environment: flags.string({
       required: true,
     }),
   }
 
   async run() {
     const {flags, argv} = this.parse(ConfigUnset)
+    const {environment} = flags
+
+    const appName = await this.resolveAppNameForEnvironment(environment)
 
     const configPairs = argv.reduce((acc, elem) => {
       return {
@@ -29,9 +32,9 @@ export default class ConfigUnset extends Command {
       }
     }, {})
 
-    cli.action.start(`Unsetting ${Object.keys(configPairs).map(key => herokuColor.configVar(key)).join(', ')} and restarting ${herokuColor.app(flags.app)}`)
+    cli.action.start(`Unsetting ${Object.keys(configPairs).map(key => herokuColor.configVar(key)).join(', ')} and restarting ${herokuColor.app(environment)}`)
 
-    await this.client.patch(`/apps/${flags.app}/config-vars`, {
+    await this.client.patch(`/apps/${appName}/config-vars`, {
       data: configPairs,
     })
 
