94f6d284b9d940536685225d640570f5198d2c4f
Chris Freeman <cfreeman.code@gmail.com>
2021-05-04 14:34:11 -0600

    Add `--json` flag for `env:list` (#29)

--- test/commands/env/list.test.ts
+++ test/commands/env/list.test.ts
@@ -79,6 +79,47 @@ const ENVIRONMENT_MOCKS = [
   },
 ]
 
+const EXPECTED_JSON_OUTPUT = {
+  org: [
+    {
+      alias: '',
+      username: 'foo@test.com',
+      orgId: '1',
+      connectedStatus: 'Connected',
+    },
+    {
+      alias: '',
+      username: 'bar@test.com',
+      orgId: '2',
+      connectedStatus: 'Connected',
+    },
+    {
+      alias: '',
+      username: 'baz@test.com',
+      orgId: '3',
+      connectedStatus: 'Connected',
+    },
+  ],
+  scratchorg: [
+    {
+      alias: 'my-scratch-org',
+      username: 'test-if5m9vhwk3av@example.com',
+      orgId: '4',
+      connectedStatus: 'Unknown',
+      expirationDate: '2021-04-12',
+    },
+  ],
+  compute: [
+    {
+      alias: '',
+      projectName: 'sweet_project',
+      connectedOrgAlias: '',
+      connectedOrgId: '1',
+      name: 'sweet-project-1',
+    },
+  ],
+}
+
 describe('sf env list', () => {
   const sandbox = sinon.createSandbox()
 
@@ -107,6 +148,29 @@ describe('sf env list', () => {
     // This assertion is verifying that the default behavior is to only show compute environments
     // associated with your projects
     expect(ctx.stdout).to.not.include('12345678')
+  })
+
+  test
+  .stderr()
+  .stdout()
+  .do(() => {
+    sandbox.stub(SfdxProject, 'resolve' as any).returns(PROJECT_MOCK)
+    sandbox.stub(EnvList.prototype, 'resolveOrgs' as any).returns(GROUPED_ORGS_MOCK)
+  })
+  .finally(() => {
+    sandbox.restore()
+  })
+  .nock('https://api.heroku.com', api => {
+    api
+    .get('/account')
+    .reply(200, HEROKU_ACCOUNT_MOCK)
+    .get(`/enterprise-accounts/${HEROKU_ACCOUNT_MOCK.salesforce_org.owner.id}/apps`)
+    .reply(200, ENVIRONMENT_MOCKS)
+  })
+  .command(['env:list', '--json'])
+  .it('returns JSON when the JSON flag is used', ctx => {
+    const jsonOutput = JSON.parse(ctx.stdout)
+    expect(jsonOutput).to.deep.equal(EXPECTED_JSON_OUTPUT)
   })
 
   test
