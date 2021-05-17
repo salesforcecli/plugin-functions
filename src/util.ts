3b207c206f26f6d3f8977c27cb55feee742d50fb
Alexandra Dalton <a.e.dalton.16@gmail.com>
2021-05-17 15:57:49 -0400

    [cleanup]: util: eslint

--- src/util.ts
+++ src/util.ts
@@ -5,7 +5,7 @@ namespace Util {
     const UUID_REGEX = /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i
     const match = spaceId.match(UUID_REGEX)
 
-    return !!match && match.length === 1
+    return Boolean(match) && match.length === 1
   }
 
   export async function getProjectDescriptor(path: string): Promise<any> {
