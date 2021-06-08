69c5a0661ce1c261b85f77dc2d33301471c6948a
mimen <miladmaaan@gmail.com>
2021-06-08 10:03:28 -0700

    delete function references when deleting compute env

--- src/commands/env/delete.ts
+++ src/commands/env/delete.ts
@@ -6,10 +6,17 @@
  */
 import herokuColor from '@heroku-cli/color';
 import * as Heroku from '@heroku-cli/schema';
-import { Org } from '@salesforce/core';
+import { AuthInfo, Org } from '@salesforce/core';
 import { cli } from 'cli-ux';
 import { FunctionsFlagBuilder, confirmationFlag } from '../../lib/flags';
 import Command from '../../lib/base';
+import {sortBy} from 'lodash'
+import {differenceWith, isEqual} from 'lodash'
+import {OrgListUtil} from '@salesforce/plugin-org/lib/shared/orgListUtil'
+interface FullNameReference {
+  project: string;
+  fn: string;
+}
 
 export default class EnvDelete extends Command {
   static description = 'delete an environment';
@@ -25,6 +32,42 @@ export default class EnvDelete extends Command {
     }),
     confirm: confirmationFlag,
   };
+
+  private filterProjectReferencesToRemove(
+    allReferences: Array<FullNameReference>,
+    successfulReferences: Array<FullNameReference>,
+    projectName: string,
+  ) {
+    const filtered = allReferences.filter(ref => ref.project === projectName)
+    return differenceWith(filtered, successfulReferences, isEqual).map(ref => `${ref.project}-${ref.fn}`)
+  }
+
+  private splitFullName(fullName: string): FullNameReference {
+    const [project, fn] = fullName.split('-')
+    return {
+      project,
+      fn,
+    }
+  }
+
+  async resolveScratchOrg(scratchOrgId: string) {
+    // adapted from https://github.com/salesforcecli/plugin-org/blob/3012cc04a670e4bf71e75a02e2f0981a71eb4e0d/src/commands/force/org/list.ts#L44-L90
+    let fileNames: Array<string> = []
+    try {
+      fileNames = await AuthInfo.listAllAuthFiles()
+    } catch (error) {
+      if (error.name === 'NoAuthInfoFound') {
+        this.error('No orgs found')
+      } else {
+        throw error
+      }
+    }
+
+    const metaConfigs = await OrgListUtil.readLocallyValidatedMetaConfigsGroupedByOrgType(fileNames, {})
+
+    const scratchOrgs = sortBy(metaConfigs.scratchOrgs, v => [v.alias, v.username])
+    return scratchOrgs.find(org => org.orgId === scratchOrgId)
+  }
 
   async run() {
     const { flags } = this.parse(EnvDelete);
@@ -59,6 +102,36 @@ export default class EnvDelete extends Command {
 
     try {
       // If app exists, it will be deleted
+
+      // Get the sales org id
+      const app = await this.client.get<Heroku.App>(`/apps/${appName}`, {
+        headers: {
+          Accept: 'application/vnd.heroku+json; version=3.evergreen',
+        },
+      })
+
+      const connectedOrg = await this.resolveScratchOrg(app.data.sales_org_connection.sales_org_id)
+      const connectedOrgAlias = connectedOrg?.alias
+      const project = await this.fetchSfdxProject()
+      const org = await this.fetchOrg(connectedOrgAlias)
+      const connection = org.getConnection()
+      console.log('here')
+      const refList = await connection.metadata.list({type: 'FunctionReference'})
+      if (refList) {
+        const allReferences = refList.reduce(
+          (acc: Array<FullNameReference>, ref) => {
+            acc.push(this.splitFullName(ref.fullName))
+            return acc
+          },
+          [],
+        )
+        console.log('here')
+        const referencesToRemove = this.filterProjectReferencesToRemove(allReferences, [], project.name)
+        console.log('here')
+        await connection.metadata.delete('FunctionReference', referencesToRemove)
+        console.log('here')
+      }
+
       await this.client.delete<Heroku.App>(`/apps/${appName}`, {
         headers: {
           Accept: 'application/vnd.heroku+json; version=3.evergreen',
