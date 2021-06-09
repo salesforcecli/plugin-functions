0ef9c977fbb2bb7dc87842dc574aec8a7bfd564b
Yousef Haggy <yhaggy@yhaggy-ltmq2wt.internal.salesforce.com>
2021-06-09 16:51:54 -0400

    Use new runFunction command from library

--- src/commands/run/function.ts
+++ src/commands/run/function.ts
@@ -1,8 +1,12 @@
 import {Command, flags} from '@oclif/command'
 import * as fs from 'fs'
 import getStdin from '../../lib/get-stdin'
-import {RunFunction, RunFunctionOptions} from '@salesforce/functions-core'
-import Util from '../../util'
+import {runFunction, RunFunctionOptions} from '@salesforce/functions-core'
+import {cli} from 'cli-ux'
+import herokuColor from '@heroku-cli/color'
+import {AxiosResponse} from 'axios'
+import {ConfigAggregator} from '@salesforce/core'
+
 export default class Invoke extends Command {
   static description = 'send a cloudevent to a function'
 
@@ -39,16 +43,33 @@ export default class Invoke extends Command {
 
   async run() {
     const {flags} = this.parse(Invoke)
-    const data = await this.getPayloadData(flags.payload)
-    if (!data) {
+    flags.payload = await this.getPayloadData(flags.payload)
+    if (!flags.payload) {
       this.error('no payload provided (provide via stdin or -p)')
     }
+    const aggregator = await ConfigAggregator.create()
+    const defaultusername = aggregator.getPropertyValue('defaultusername');
+    if (!flags.targetusername && !defaultusername) {
+      this.warn('No -t targetusername or defaultusername found, context will be partially initialized')
+    }
+    const aliasOrUser = flags.targetusername || `defaultusername ${defaultusername}`
+    this.log(`Using ${aliasOrUser} login credential to initialize context`)
 
-    const runFunction = new RunFunction()
-
-    Util.outputSfFunctionCommandEvents(runFunction)
-
-    runFunction.execute(flags as RunFunctionOptions)
+    cli.action.start(`${herokuColor.cyanBright('POST')} ${flags.url}`)
+    try {
+      const response = await runFunction(flags as RunFunctionOptions)
+      this.writeResponse(response)
+    } catch (error) {
+      cli.debug(error)
+      if (error.response) {
+        cli.action.stop(herokuColor.redBright(`${error.response.status} ${error.response.statusText}`))
+        this.debug(error.response)
+        this.error(error.response.data)
+      } else {
+        cli.action.stop(herokuColor.redBright('Error'))
+        this.error(error)
+      }
+    }
   }
 
   async getPayloadData(payload: string | undefined): Promise<string | undefined> {
@@ -57,4 +78,17 @@ export default class Invoke extends Command {
     }
     return payload || getStdin()
   }
+
+  writeResponse(response: AxiosResponse) {
+    const contentType = response.headers['content-type']
+    if (
+      contentType.includes('application/json') ||
+      contentType.includes('application/cloudevents+json')
+    ) {
+      cli.styledJSON(response.data)
+    } else {
+      this.log(response.data)
+    }
+  }
 }
+
