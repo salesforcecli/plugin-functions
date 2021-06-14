00c859df94d49b27bc5e870e176552eca9ad16f7
mimen <miladmaaan@gmail.com>
2021-06-14 15:45:13 -0700

    fix(cli): refactored shared functions to util

--- test/helpers/init.js
+++ test/helpers/init.js
@@ -1,13 +1,13 @@
-const {color} = require('@heroku-cli/color')
-const chai = require('chai')
-const sinonChai = require('sinon-chai')
+const { color } = require('@heroku-cli/color');
+const chai = require('chai');
+const sinonChai = require('sinon-chai');
 
-chai.use(sinonChai)
+chai.use(sinonChai);
 
-color.enabled = false
-process.env.FORCE_COLOR = '0'
+color.enabled = false;
+process.env.FORCE_COLOR = '0';
 
-process.stdout.columns = 80
-process.stderr.columns = 80
+process.stdout.columns = 80;
+process.stderr.columns = 80;
 
-process.env.CLI_UX_SKIP_TTY_CHECK = true
+process.env.CLI_UX_SKIP_TTY_CHECK = true;
