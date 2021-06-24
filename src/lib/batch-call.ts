1970264a6be0514ed5cb93a02c9ff10cffc39dc9
Chris Freeman <cfreeman.code@gmail.com>
2021-06-24 10:19:07 -0600

    fix: upsert function references sequentially (#94)

---
+++ src/lib/batch-call.ts
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2020, salesforce.com, inc.
+ * All rights reserved.
+ * Licensed under the BSD 3-Clause license.
+ * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ */
+import { chunk } from 'lodash';
+import { ensureArray } from './function-reference-utils';
+
+export async function batchCall<T, U>(data: T[], fn: (chunk: T[]) => Promise<U[] | U>) {
+  const chunked = chunk(data, 10);
+  let results: U[] = [];
+
+  for (const chunk of chunked) {
+    const result = await fn(chunk);
+
+    results = [...results, ...ensureArray(result)];
+  }
+
+  return results;
+}
+
+export default batchCall;
