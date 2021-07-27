d5d928d9941c8ba2d50cf99bff74a38983a9c203
YousefHaggyHeroku <84479345+YousefHaggyHeroku@users.noreply.github.com>
2021-07-27 19:39:08 -0400

    chore: Use @heroku/functions-core for generate commands (#116)
    * chore: use @heroku/functions-core for generate commands
    
    * chore: update package.json
    
    * chore: update functions-core version

--- src/commands/generate/project.ts
+++ src/commands/generate/project.ts
@@ -4,21 +4,9 @@
  * Licensed under the BSD 3-Clause license.
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
-import * as path from 'path';
 import { Flags } from '@oclif/core';
-import ProjectGenerator from '@salesforce/templates/lib/generators/projectGenerator';
-import * as fs from 'fs-extra';
-import { createEnv } from 'yeoman-environment';
+import { generateProject } from '@heroku/functions-core';
 import Command from '../../lib/base';
-
-const options = {
-  outputdir: '.',
-  template: 'standard',
-  loginurl: 'https://login.salesforce.com',
-  defaultpackagedir: 'force-app',
-  ns: '',
-};
-
 export default class GenerateProject extends Command {
   static flags = {
     name: Flags.string({
@@ -30,39 +18,10 @@ export default class GenerateProject extends Command {
 
   async run() {
     const { flags } = await this.parse(GenerateProject);
-
-    if (flags.name.includes('-')) {
-      this.error('Project names may not include hyphens, please either remove them or use underscores.');
-    }
-
-    const projectPath = path.resolve(`./${flags.name}`);
-
-    if (await fs.pathExists(projectPath)) {
-      this.error(`Directory ${flags.name} already exists.`);
-    }
-
-    const env = createEnv();
-    env.registerStub(ProjectGenerator, 'project-generator');
-
     try {
-      await env.run('project-generator', { ...options, projectname: flags.name }, (err) => {
-        if (err) {
-          this.error(err);
-        }
-      });
-    } catch (error) {
-      // This is an error that is specific to the yeoman-environment package. It comes up whenever
-      // someone has a package.json in the directory they are trying to generate a project in, or
-      // one of its ancestors. The default error message is also complete useless, so we'll catch it
-      // instead of provide useful feedback
-      if (error.message?.includes('A name parameter is required to create a storage')) {
-        this.error(
-          'There was an issue generating the project. Please ensure there are no package.json ' +
-            'files in this directory or any of its parents.'
-        );
-      }
-
-      throw error;
+      await generateProject(flags.name);
+    } catch (err) {
+      this.error(err.message);
     }
   }
 }
--- test/commands/generate/function.test.ts
+++ test/commands/generate/function.test.ts
@@ -4,211 +4,64 @@
  * Licensed under the BSD 3-Clause license.
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
-import * as path from 'path';
 import { expect, test } from '@oclif/test';
-import * as fs from 'fs-extra';
 import * as sinon from 'sinon';
-import * as pathUtils from '../../../src/lib/path-utils';
-import * as javaNameUtils from '../../../src/lib/java-name-utils';
+import * as library from '@heroku/functions-core';
 
 describe('sf generate:function', () => {
   const sandbox: sinon.SinonSandbox = sinon.createSandbox();
-
-  function testTemplate(template: string, sfdxProjectPath: string | null) {
-    return test
-      .stub(pathUtils, 'resolveSfdxProjectPath', () => {
-        if (sfdxProjectPath) {
-          return sfdxProjectPath;
-        }
-
-        throw new Error('no project path');
-      })
-      .stub(fs, 'mkdirpSync', sandbox.stub())
-      .stub(fs, 'outputFileSync', sandbox.stub())
-      .stub(pathUtils, 'resolveFunctionsPaths', sandbox.stub())
-      .stdout()
-      .stderr()
-      .finally(() => {
-        sandbox.restore();
-      })
-      .command(['generate:function', '--name=myfn', `--language=${template}`]);
-  }
-
-  // Javascript
-  const javascriptBasicTemplateFiles = 7;
-  testTemplate('javascript', 'sfdx-project.json').it('generates a javascript function', (ctx) => {
-    expect(ctx.stderr).to.equal('');
-    expect(ctx.stdout).to.contain('Created javascript');
-    expect(fs.mkdirpSync).to.be.called;
-    expect(fs.outputFileSync).to.have.callCount(javascriptBasicTemplateFiles);
+  let generateFunctionStub: sinon.SinonStub;
+  const path = 'some/functions/path';
+  const name = 'myfunction';
+  const language = 'javascript';
+  beforeEach(() => {
+    generateFunctionStub = sandbox.stub(library, 'generateFunction');
   });
-
-  // Typescript
-  const typescriptBasicTemplateFiles = 8;
-  testTemplate('typescript', 'sfdx-project.json').it('generates a typescript function', (ctx) => {
-    expect(ctx.stderr).to.equal('');
-    expect(ctx.stdout).to.contain('Created typescript');
-    expect(fs.mkdirpSync).to.be.called;
-    expect(fs.outputFileSync).to.have.callCount(typescriptBasicTemplateFiles);
-  });
-
-  testTemplate('javascript', null)
-    .catch((error) => {
-      expect(error.message).to.contain('sf generate function must be run inside an sfdx project');
-    })
-    .it('does not generate a function when it is not run inside a project', (ctx) => {
-      expect(ctx.stderr).to.equal('');
-      expect(ctx.stdout).to.not.contain('Created javascript');
-      expect(fs.mkdirpSync).to.not.have.been.called;
-      expect(fs.outputFileSync).to.not.have.been.called;
-    });
-
-  testTemplate('typescript', path.join('..', '..', '..', 'sfdx-project.json')).it(
-    'generates a function even if called from below the root of a project',
-    () => {
-      const expectedPath = path.join('..', '..', '..', 'functions', 'myfn', 'index.ts');
-      expect(fs.outputFileSync).to.have.been.calledWith(expectedPath);
-    }
-  );
-
-  testTemplate('javascript', 'sfdx-project.json').stub(pathUtils, 'resolveFunctionsPaths', () => {
-    throw new Error('No functions directory found');
+  afterEach(() => {
+    sandbox.restore();
   });
 
   test
-    .command(['generate:function', '--name=1fn', '--language=javascript'])
-    .catch((error) => {
-      expect(error.message).to.include('Function names must');
-    })
-    .it('does not allow a function name that starts with a number');
-
-  test
-    .command(['generate:function', '--name=fn-1', '--language=javascript'])
-    .catch((error) => {
-      expect(error.message).to.include('Function names must');
-    })
-    .it('does not allow a function name that contains a hyphen');
-
-  test
-    .command(['generate:function', '--name=fn_1', '--language=javascript'])
-    .catch((error) => {
-      expect(error.message).to.include('Function names must');
-    })
-    .it('does not allow a function name that contains an underscore');
-
-  test
-    .command(['generate:function', '--name=fn_1', '--language=javascript'])
-    .catch((error) => {
-      expect(error.message).to.include('Function names must');
-    })
-    .it('does not allow a function name that contains an underscore');
-
-  test
-    .command(['generate:function', '--name=FN', '--language=javascript'])
-    .catch((error) => {
-      expect(error.message).to.include('Function names must');
-    })
-    .it('does not allow a function name that contains capital letters');
-
-  test
-    .command(['generate:function', `--name=${'x'.repeat(48)}`, '--language=javascript'])
-    .catch((error) => {
-      expect(error.message).to.include('Function names cannot contain more than 47 characters.');
-    })
-    .it('does not allow a function name that contains more than 47 characters');
-
-  test
-    .stub(pathUtils, 'resolveSfdxProjectPath', () => 'sfdx-project.json')
-    .stub(fs, 'mkdirpSync', sandbox.stub())
-    .stub(fs, 'outputFileSync', sandbox.stub())
-    .stub(pathUtils, 'resolveFunctionsPaths', sandbox.stub().throws({ message: 'No functions directory found.' }))
     .stdout()
-    .stderr()
-    .finally(() => {
-      sandbox.restore();
+    .do(() => {
+      generateFunctionStub.returns({
+        name,
+        path,
+        language,
+        welcomeText: '',
+      });
     })
-    .command(['generate:function', '--name=myfn', '--language=javascript'])
-    .it('prints welcome message', (ctx) => {
-      expect(ctx.stdout).to.contain('Before creating Scratch Orgs for development, please ensure that:');
+    .command(['generate:function', `--name=${name}`, '--language=javascript'])
+    .it('Should call the library methods with proper args and log output', async (ctx) => {
+      expect(generateFunctionStub).to.have.been.calledWith(name, language);
+      expect(ctx.stdout).to.contain(`Created ${language} function ${name} in ${path}`);
     });
-});
 
-describe('sf generate:function --language java', () => {
-  const sandbox: sinon.SinonSandbox = sinon.createSandbox();
+  test
+    .do(() => {
+      generateFunctionStub.returns({
+        name,
+        path,
+        language,
+        welcomeText: 'Before each...',
+      });
+    })
+    .stdout()
+    .command(['generate:function', `--name=${name}`, '--language=javascript'])
+    .it('Should log welcome message', async (ctx) => {
+      expect(generateFunctionStub).to.have.been.calledWith('myfunction', 'javascript');
+      expect(ctx.stdout).to.contain('Before each...');
+    });
 
-  function testJavaTemplate(sfdxProjectPath: string | null) {
-    return test
-      .stub(pathUtils, 'resolveSfdxProjectPath', () => {
-        if (sfdxProjectPath) {
-          return sfdxProjectPath;
-        }
-
-        throw new Error('no project path');
-      })
-      .stub(fs, 'mkdirpSync', sandbox.spy())
-      .stub(fs, 'copySync', sandbox.spy())
-      .stub(fs, 'outputFileSync', sandbox.spy())
-      .stub(pathUtils, 'resolveFunctionsPaths', sandbox.stub())
-      .stdout()
-      .stderr()
-      .finally(() => {
-        // We cannot effectively stub all fs calls for Java template tests and we need to clean up after the test
-        if (sfdxProjectPath) {
-          fs.removeSync(path.join(path.dirname(sfdxProjectPath), 'functions', 'MyFunction'));
-        }
-
-        sandbox.restore();
-      })
-      .command(['generate:function', '--name=myfn', '--language=java']);
-  }
-
-  const javaBasicFiles = 7;
-  const javaTemplateFiles = 4;
-  testJavaTemplate('sfdx-project.json').it('generates a java function', (ctx) => {
-    expect(ctx.stderr).to.equal('');
-    expect(ctx.stdout).to.contain('Created java');
-    expect(fs.mkdirpSync).to.have.been.called;
-    expect(fs.copySync).to.have.callCount(javaBasicFiles);
-    expect(fs.outputFileSync).to.have.callCount(javaTemplateFiles);
-  });
-});
-
-describe('toJavaClassName', () => {
-  it('removes illegal characters from the name', () => {
-    expect(javaNameUtils.toJavaClassName('Hello👋')).to.equal('Hello');
-    expect(javaNameUtils.toJavaClassName('FoöBär')).to.equal('FoBr');
-    expect(javaNameUtils.toJavaClassName('FooÄaa')).to.equal('Fooaa');
-  });
-
-  it('removes spaces and capitalizes the next word after a space', () => {
-    expect(javaNameUtils.toJavaClassName('Hello world')).to.equal('HelloWorld');
-  });
-
-  it('does not mangle already camel-cased names', () => {
-    expect(javaNameUtils.toJavaClassName('HelloWorld function')).to.equal('HelloWorldFunction');
-  });
-
-  it('adds a prefix when the first character is not a letter', () => {
-    expect(javaNameUtils.toJavaClassName('99 Red Balloons')).to.equal('A99RedBalloons');
-  });
-});
-
-describe('toMavenArtifactId', () => {
-  it('removes illegal characters from the name', () => {
-    expect(javaNameUtils.toMavenArtifactId('Hello👋')).to.equal('hello');
-    expect(javaNameUtils.toMavenArtifactId('FoöBär')).to.equal('fo-br');
-    expect(javaNameUtils.toMavenArtifactId('FooÄaa')).to.equal('fooaa');
-  });
-
-  it('replaces spaces with dashes', () => {
-    expect(javaNameUtils.toMavenArtifactId('Hello world Function')).to.equal('hello-world-function');
-  });
-
-  it('replaces multiple spaces with only one dash', () => {
-    expect(javaNameUtils.toMavenArtifactId('Hello    world')).to.equal('hello-world');
-  });
-
-  it('does not mangle uppercase names like acronyms', () => {
-    expect(javaNameUtils.toMavenArtifactId('There are too many TLAs')).to.equal('there-are-too-many-tlas');
-  });
+  test
+    .do(() => {
+      generateFunctionStub.rejects(new Error('something bad happened'));
+    })
+    .command(['generate:function', `--name=${name}`, '--language=javascript'])
+    .catch((error) => {
+      expect(error.message).to.contain('something bad happened');
+    })
+    .it('Should log errors', async (ctx) => {
+      expect(generateFunctionStub).to.have.been.calledWith('myfunction', 'javascript');
+    });
 });
