107e47425f698857f9b4405a8022358b714b3217
Zane Whitfield <zwhitfield@salesforce.com>
2021-09-13 11:25:00 -0700

    fix: add json flag to whoami:functions (#197)
    * fix: add json flag and format
    
    * fix: add json to markdown file
    
    * fix: update snapshot file
    
    * fix: update json logic & tests

--- src/commands/whoami/functions.ts
+++ src/commands/whoami/functions.ts
@@ -5,6 +5,7 @@
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
 import { Flags } from '@oclif/core';
+import { cli } from 'cli-ux';
 import { dim, cyan } from 'chalk';
 import { Messages } from '@salesforce/core';
 import Command from '../../lib/base';
@@ -27,6 +28,10 @@ export default class WhoAmI extends Command {
       description: messages.getMessage('flags.show-token.summary'),
       hidden: true,
     }),
+    json: Flags.boolean({
+      description: messages.getMessage('flags.json.summary'),
+      char: 'j',
+    }),
   };
 
   async run(): Promise<FunctionsInformation> {
@@ -34,21 +39,33 @@ export default class WhoAmI extends Command {
     const ret: FunctionsInformation = {};
     const account = await this.fetchAccount();
 
-    this.log(`Hello ${account.name} 👋 \n`);
-
-    if (flags['show-token']) {
-      this.log(`Your functions token is: ${cyan(this.auth)}\n`);
-      ret.token = this.auth;
-    }
-
-    this.log('Here is some information on your functions account:');
-
     const fields = Object.entries(account).filter(([key]) => FIELDS.includes(key as FunctionsInformationKey));
-    const pad = fields.reduce((max, [key]) => (key.length > max ? key.length : max), 0) + 2;
-    fields.forEach(([key, val]) => {
-      ret[key as FunctionsInformationKey] = val;
-      this.log(dim(`  ${(key + ' :').padStart(pad)} ${val}`));
-    });
+
+    if (flags.json) {
+      if (flags['show-token']) {
+        ret.token = this.auth;
+      }
+      fields.forEach(([key, val]) => {
+        ret[key as FunctionsInformationKey] = val;
+      });
+      cli.styledJSON(ret);
+    } else {
+      this.log(`Hello ${account.name} 👋 \n`);
+
+      if (flags['show-token']) {
+        this.log(`Your functions token is: ${cyan(this.auth)}\n`);
+        ret.token = this.auth;
+      }
+
+      this.log('Here is some information on your functions account:');
+
+      const pad = fields.reduce((max, [key]) => (key.length > max ? key.length : max), 0) + 2;
+
+      fields.forEach(([key, val]) => {
+        ret[key as FunctionsInformationKey] = val;
+        this.log(dim(`  ${(key + ' :').padStart(pad)} ${val}`));
+      });
+    }
     return ret;
   }
 }
