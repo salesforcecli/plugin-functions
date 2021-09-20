d1eb96c872510a30ce734bb420c6ea6662f08aa9
Chris Freeman <cfreeman.code@gmail.com>
2021-09-20 14:14:42 -0600

    feat: add logout:functions command

---
+++ src/commands/logout/functions.ts
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2020, salesforce.com, inc.
+ * All rights reserved.
+ * Licensed under the BSD 3-Clause license.
+ * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ */
+import { cli } from 'cli-ux';
+import { Messages } from '@salesforce/core';
+
+import Command from '../../lib/base';
+
+Messages.importMessagesDirectory(__dirname);
+const messages = Messages.loadMessages('@salesforce/plugin-functions', 'logout.functions');
+
+export default class Login extends Command {
+  static description = messages.getMessage('summary');
+
+  static examples = messages.getMessages('examples');
+
+  async run() {
+    cli.action.start(messages.getMessage('action.start'));
+
+    this.info.unsetToken(Command.TOKEN_BEARER_KEY);
+    await this.info.write();
+
+    cli.action.stop();
+  }
+}
