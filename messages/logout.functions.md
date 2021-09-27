133211ad430126bde3507b9d94e00f6792fa4ba6
Chris Freeman <cfreeman.code@gmail.com>
2021-09-27 12:20:33 -0600

    feat: add logout:functions command (#216) (#233)
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    Co-authored-by: peternhale <peternhale@users.noreply.github.com>

---
+++ messages/logout.functions.md
@@ -0,0 +1,11 @@
+# summary
+
+Log out of your Salesforce Functions account.
+
+# examples
+
+$ sfdx logout:functions
+
+# action.start
+
+Logging out of Salesforce Functions
---
+++ test/commands/logout/functions.test.ts
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2020, salesforce.com, inc.
+ * All rights reserved.
+ * Licensed under the BSD 3-Clause license.
+ * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ */
+import { test } from '@oclif/test';
+import type { GlobalInfo, SfTokens } from '@salesforce/core';
+import * as sinon from 'sinon';
+import { AuthStubs } from '../../helpers/auth';
+
+describe('sf logout functions', () => {
+  let contents: SfTokens;
+
+  beforeEach(() => {
+    AuthStubs.write.callsFake(async function (this: GlobalInfo) {
+      contents = this.getTokens(true);
+      return this.getContents();
+    });
+  });
+
+  test
+    .stdout()
+    .stderr()
+    .command(['logout:functions'])
+    .it('removes the functions key from the tokens field on logout', (ctx) => {
+      sinon.assert.match(contents, {});
+    });
+});
