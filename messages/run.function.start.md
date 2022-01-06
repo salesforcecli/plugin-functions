a6c2ce4bc5284d34e7cca61d21c21828cf895d29
Josh W Lewis <josh.lewis@salesforce.com>
2022-01-06 16:19:35 -0600

    chore: update run:function:start docs
    The help text should now reflect the upcoming changes and provide
    instructions to use the subcommands if desired.

---
+++ messages/run.function.start.md
@@ -0,0 +1,9 @@
+# summary
+
+Build and run a Salesforce Function.
+
+# description
+
+Run this command from the directory of your Salesforce Functions project.
+
+This command will run the target function in a container. In the future, this command will run the target function on the host operating system (locally) instead. If one mode is preferred over the other, consider using the `container` or `local` subcommand instead.
--- src/commands/run/function/start.ts
+++ src/commands/run/function/start.ts
@@ -5,6 +5,14 @@
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
 
-// run:function:start is an alias to run:function:start:container
+import { Messages } from '@salesforce/core';
 import Container from './start/container';
-export default class Start extends Container {}
+
+Messages.importMessagesDirectory(__dirname);
+const messages = Messages.loadMessages('@salesforce/plugin-functions', 'run.function.start');
+
+// run:function:start is an alias to run:function:start:container
+export default class Start extends Container {
+  static summary = messages.getMessage('summary');
+  static description = messages.getMessage('description');
+}
