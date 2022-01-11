44a3e6103a43f719d55eb6e0ea13ca0c460add70
Josh W Lewis <josh.lewis@salesforce.com>
2022-01-11 15:51:35 -0600

    feat: run:function:start now defaults to local
    It is now an alias to run:function:start:local rather than
    run:function:start:container.

--- messages/run.function.start.md
+++ messages/run.function.start.md
@@ -6,4 +6,58 @@ Build and run a Salesforce Function.
 
 Run this command from the directory of your Salesforce Functions project.
 
-This command will run the target function in a container. In the future, this command will run the target function on the host operating system (locally) instead. If one mode is preferred over the other, consider using the `container` or `local` subcommand instead.
+This command will run the target function locally (on the same operating system as this CLI), just like the `local` subcommand.
+
+Previously, this command ran functions in a container. Container mode is still supported via the `container` subcommand. Arguments relevant to container mode are still accepted, but are deprecated, ignored, and will be dropped in a future release.
+
+# flags.builder.summary
+
+Set custom builder image. Deprecated.
+
+# flags.clear-cache.summary
+
+Clear associated cache before executing. Deprecated.
+
+# flags.debug-port.summary
+
+Port for remote debugging.
+
+# flags.descriptor.summary
+
+Path to project descriptor file (project.toml) that contains function and/or bulid configuration. Deprecated.
+
+# flags.env.summary
+
+Set environment variables (provided during build and run). Deprecated.
+
+# flags.language.summary
+
+The language that the function runs in.
+
+# flags.network.summary
+
+Connect and build containers to a network. This can be useful to build containers which require a local resource. Deprecated.
+
+# flags.no-build.summary
+
+Skip building the an image. Deprecated.
+
+# flags.no-pull.summary
+
+Skip pulling builder image before use. Deprecated.
+
+# flags.no-run.summary
+
+Skip running the built image. Deprecated.
+
+# flags.path.summary
+
+Path to function directory.
+
+# flags.port.summary
+
+Port for running the function.
+
+# flags.verbose.summary
+
+Output additional logs.
--- src/commands/run/function/start.ts
+++ src/commands/run/function/start.ts
@@ -5,14 +5,78 @@
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
 
+import * as path from 'path';
 import { Messages } from '@salesforce/core';
-import Container from './start/container';
+import { Flags } from '@oclif/core';
+import Local from './start/local';
 
 Messages.importMessagesDirectory(__dirname);
 const messages = Messages.loadMessages('@salesforce/plugin-functions', 'run.function.start');
 
-// run:function:start is an alias to run:function:start:container
-export default class Start extends Container {
+// run:function:start is an alias to run:function:start:local.
+// run:function:start previously ran via container mode, so it accepts
+// arguments applicable to the container subcommand, but ignores them and flags
+// them as deprecated. The additional flags may be removed after 04/30/2022.
+export default class Start extends Local {
   static summary = messages.getMessage('summary');
+
   static description = messages.getMessage('description');
+
+  static flags = {
+    builder: Flags.string({
+      description: messages.getMessage('flags.builder.summary'),
+      hidden: true,
+    }),
+    'clear-cache': Flags.boolean({
+      description: messages.getMessage('flags.clear-cache.summary'),
+    }),
+    'debug-port': Flags.integer({
+      char: 'b',
+      description: messages.getMessage('flags.debug-port.summary'),
+      default: 9229,
+    }),
+    descriptor: Flags.string({
+      description: messages.getMessage('flags.descriptor.summary'),
+      hidden: true,
+    }),
+    env: Flags.string({
+      char: 'e',
+      description: messages.getMessage('flags.env.summary'),
+      multiple: true,
+    }),
+    language: Flags.enum({
+      options: ['javascript', 'typescript', 'java', 'auto'],
+      description: messages.getMessage('flags.language.summary'),
+      char: 'l',
+      default: 'auto',
+    }),
+    network: Flags.string({
+      description: messages.getMessage('flags.network.summary'),
+    }),
+    'no-build': Flags.boolean({
+      description: messages.getMessage('flags.no-build.summary'),
+      hidden: true,
+    }),
+    'no-pull': Flags.boolean({
+      description: messages.getMessage('flags.no-pull.summary'),
+    }),
+    'no-run': Flags.boolean({
+      description: messages.getMessage('flags.no-run.summary'),
+      hidden: true,
+    }),
+    path: Flags.string({
+      description: messages.getMessage('flags.path.summary'),
+      default: path.resolve('.'),
+      hidden: true,
+    }),
+    port: Flags.integer({
+      char: 'p',
+      description: messages.getMessage('flags.port.summary'),
+      default: 8080,
+    }),
+    verbose: Flags.boolean({
+      char: 'v',
+      description: messages.getMessage('flags.verbose.summary'),
+    }),
+  };
 }
