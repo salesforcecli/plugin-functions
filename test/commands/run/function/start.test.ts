dc8cdb68b94029e31c9c47a23af5638ed71b484d
Josh W Lewis <josh.lewis@salesforce.com>
2022-01-13 10:12:23 -0600

    chore: test deprecation notices

--- test/commands/run/function/start.test.ts
+++ test/commands/run/function/start.test.ts
@@ -4,33 +4,46 @@
  * Licensed under the BSD 3-Clause license.
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
-import { test } from '@oclif/test';
+import { expect, test } from '@oclif/test';
 import * as sinon from 'sinon';
+import { LocalRun } from '@heroku/functions-core';
 
-import * as library from '@heroku/functions-core';
-import { Benny } from '@heroku/functions-core';
-
-describe('function:start', () => {
+describe('run:function:start', () => {
   let sandbox: sinon.SinonSandbox;
-  let bennyRunStub: sinon.SinonStub;
-  let bennyBuildStub: sinon.SinonStub;
+  let localRunExecStub: sinon.SinonStub;
   beforeEach(() => {
     sandbox = sinon.createSandbox();
-    bennyRunStub = sandbox.stub(Benny.prototype, 'run');
-    bennyBuildStub = sandbox.stub(Benny.prototype, 'build');
-    bennyRunStub.returns(true);
-    bennyBuildStub.returns(true);
-    sandbox
-      .stub(library, 'getProjectDescriptor')
-      .returns(Promise.resolve({ com: { salesforce: { id: 'allthethingsfunction' } } }));
+    localRunExecStub = sandbox.stub(LocalRun.prototype, 'exec');
+    localRunExecStub.resolves();
   });
 
   afterEach(() => {
     sandbox.restore();
   });
 
-  test.command(['run:function:start']).it('Should call the library methods', async () => {
-    sinon.assert.calledOnce(bennyBuildStub);
-    sinon.assert.calledOnce(bennyRunStub);
+  test.command(['run:function:start']).it('Should call LocalRun.exec', async () => {
+    sinon.assert.calledOnce(localRunExecStub);
+  });
+
+  ['--builder', '--network', '--env'].forEach((deprecatedArg) => {
+    describe(`with deprecated arg ${deprecatedArg}`, () => {
+      test
+        .stderr()
+        .command(['run:function:start', deprecatedArg, 'some:val'])
+        .it('will include a deprecation notice', (ctx) => {
+          expect(ctx.stderr).to.contain(`${deprecatedArg} is deprecated`);
+        });
+    });
+  });
+
+  ['--no-pull', '--no-run', '--no-build'].forEach((deprecatedArg) => {
+    describe(`with deprecated flag ${deprecatedArg}`, () => {
+      test
+        .stderr()
+        .command(['run:function:start', deprecatedArg])
+        .it('will include a deprecation notice', (ctx) => {
+          expect(ctx.stderr).to.contain(`${deprecatedArg} is deprecated`);
+        });
+    });
   });
 });
