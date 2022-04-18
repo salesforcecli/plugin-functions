71a6d6bff263cd90132775b0e453f25160731371
Sara D. Cope <scope@salesforce.com>
2022-04-17 20:01:38 -0400

    chore: update tests

--- test/commands/env/var/unset.test.ts
+++ test/commands/env/var/unset.test.ts
@@ -4,10 +4,23 @@
  * Licensed under the BSD 3-Clause license.
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
+import { CLIError } from '@oclif/core/lib/errors';
 import { expect, test } from '@oclif/test';
 import vacuum from '../../../helpers/vacuum';
 
 describe('sf env:var:unset', () => {
+  const jsonSuccess = {
+    status: 0,
+    result: null,
+    warnings: [],
+  };
+  const jsonAppError = {
+    status: 1,
+    name: 'Error',
+    message: "Couldn't find that app <my-environment>",
+    warnings: [],
+  };
+
   test
     .stdout()
     .stderr()
@@ -26,6 +39,43 @@ describe('sf env:var:unset', () => {
     .command(['env:var:unset', 'foo', '--target-compute', 'my-environment'])
     .it('works with a single variable', (ctx) => {
       expect(ctx.stderr).to.contain('Unsetting foo and restarting my-environment');
+    });
+
+  test
+    .stdout()
+    .stderr()
+    .nock('https://api.heroku.com', (api) =>
+      api
+        .patch('/apps/my-environment/config-vars', {
+          foo: null,
+        })
+        .reply(200)
+        .get('/apps/my-environment/config-vars')
+        .reply(200, {
+          foo: 'bar',
+          baz: 'baq',
+        })
+    )
+    .command(['env:var:unset', 'foo', '--target-compute', 'my-environment', '--json'])
+    .it('will show json output with success', (ctx) => {
+      const succJSON = JSON.parse(ctx.stdout);
+
+      expect(succJSON.status).to.deep.equal(jsonSuccess.status);
+      expect(succJSON.result).to.eql(jsonSuccess.result);
+    });
+
+  test
+    .stdout()
+    .stderr()
+    .command(['env:var:unset', 'foo', '--target-compute', 'my-environment', '--json'])
+    .catch((error) => {
+      expect((error as CLIError).oclif.exit).to.equal(1);
+    })
+    .it('will show json output with app error', (ctx) => {
+      const errJSON = JSON.parse(ctx.stdout);
+
+      expect(errJSON.status).to.eql(jsonAppError.status);
+      expect(errJSON.message).to.eql(jsonAppError.message);
     });
 
   test
@@ -82,33 +132,9 @@ describe('sf env:var:unset', () => {
 
   test
     .stdout()
-    .nock('https://api.heroku.com', (api) =>
-      api
-        .patch('/apps/my-environment/config-vars', {
-          foo: null,
-        })
-        .reply(200)
-        .get('/apps/my-environment/config-vars')
-        .reply(200, {
-          foo: 'bar',
-          baz: 'baq',
-        })
-    )
-    .command(['env:var:unset', 'foo', '--environment', 'my-environment', '--json'])
-    .it('will show json output', (ctx) => {
-      expect(vacuum(ctx.stdout).replace(/\n[›»]/gm, '')).to.contain(
-        vacuum('{\n"status": 0,\n"result": null,\n"warnings": []\n}')
-      );
-    });
-
-  test
-    .stdout()
-    .command(['env:var:unset', 'foo', '--environment', 'my-environment2', '--json'])
-    .it('will show json output error with incorrect compute environment', (ctx) => {
-      expect(vacuum(ctx.stdout).replace(/\n[›»]/gm, '')).to.contain(
-        vacuum(
-          '{\n"status": 1,\n"message": "Value provided for key does not match a config var found for environment.",\n"name": "Error",\n"warnings: []",'
-        )
-      );
-    });
+    .command(['env:var:unset', 'foo', '--environment', 'my-environment2'])
+    .catch((error) => {
+      expect(error.message).to.contain("Couldn't find that app <my-environment2>");
+    })
+    .it('errors with incorrect compute environment');
 });
