f1e32bba1d6465920ffbf7aad1bde6bc748a46ff
Zane Whitfield <zwhitfield@salesforce.com>
2022-05-20 06:17:26 -0700

    fix: Add json flag to sf env var unset (#316)
    * fix: add json functionality and tests
    
    * chore: update snapshot
    
    * fix: update json functionality
    
    * chore: update test
    
    * fix: better error handling
    
    * chore: update tests
    
    * fix: pull in error base
    
    Co-authored-by: Sara D. Cope <scope@salesforce.com>

--- test/commands/env/var/unset.test.ts
+++ test/commands/env/var/unset.test.ts
@@ -4,10 +4,23 @@
  * Licensed under the BSD 3-Clause license.
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
+import { CLIError } from '@oclif/core/lib/errors';
 import { expect, test } from '@oclif/test';
 import vacuum from '../../../helpers/vacuum';
 
 describe('sf env:var:unset', () => {
+  const jsonSuccess = {
+    status: 0,
+    result: null,
+    warnings: [],
+  };
+  const jsonAppError = {
+    status: 1,
+    name: 'Error',
+    message: "Couldn't find that app <my-environment>",
+    warnings: [],
+  };
+
   test
     .stdout()
     .stderr()
@@ -17,6 +30,11 @@ describe('sf env:var:unset', () => {
           foo: null,
         })
         .reply(200)
+        .get('/apps/my-environment/config-vars')
+        .reply(200, {
+          foo: 'bar',
+          baz: 'baq',
+        })
     )
     .command(['env:var:unset', 'foo', '--target-compute', 'my-environment'])
     .it('works with a single variable', (ctx) => {
@@ -30,9 +48,51 @@ describe('sf env:var:unset', () => {
       api
         .patch('/apps/my-environment/config-vars', {
           foo: null,
+        })
+        .reply(200)
+        .get('/apps/my-environment/config-vars')
+        .reply(200, {
+          foo: 'bar',
+          baz: 'baq',
+        })
+    )
+    .command(['env:var:unset', 'foo', '--target-compute', 'my-environment', '--json'])
+    .it('will show json output with success', (ctx) => {
+      const succJSON = JSON.parse(ctx.stdout);
+
+      expect(succJSON.status).to.deep.equal(jsonSuccess.status);
+      expect(succJSON.result).to.eql(jsonSuccess.result);
+    });
+
+  test
+    .stdout()
+    .stderr()
+    .command(['env:var:unset', 'foo', '--target-compute', 'my-environment', '--json'])
+    .catch((error) => {
+      expect((error as CLIError).oclif.exit).to.equal(1);
+    })
+    .it('will show json output with app error', (ctx) => {
+      const errJSON = JSON.parse(ctx.stdout);
+
+      expect(errJSON.status).to.eql(jsonAppError.status);
+      expect(errJSON.message).to.eql(jsonAppError.message);
+    });
+
+  test
+    .stdout()
+    .stderr()
+    .nock('https://api.heroku.com', (api) =>
+      api
+        .patch('/apps/my-environment/config-vars', {
+          foo: null,
           bar: null,
         })
         .reply(200)
+        .get('/apps/my-environment/config-vars')
+        .reply(200, {
+          foo: 'bar',
+          baz: 'baq',
+        })
     )
     .command(['env:var:unset', 'foo', 'bar', '--target-compute', 'my-environment'])
     .it('works with a multiple variables', (ctx) => {
@@ -47,6 +107,11 @@ describe('sf env:var:unset', () => {
           foo: null,
         })
         .reply(200)
+        .get('/apps/my-environment/config-vars')
+        .reply(200, {
+          foo: 'bar',
+          baz: 'baq',
+        })
     )
     .command(['env:var:unset', 'foo', '--environment', 'my-environment'])
     .it('will use a compute environment if passed using the old flag (not --target-compute)', (ctx) => {
@@ -61,7 +126,15 @@ describe('sf env:var:unset', () => {
     .stderr()
     .command(['env:var:unset', '--target-compute', 'my-environment'])
     .catch((error) => {
-      expect(error.message).to.contain('you must enter a config var key (i.e. mykey)');
+      expect(error.message).to.contain('You must enter a config var key (i.e. mykey).');
     })
     .it('errors when no argument is given');
+
+  test
+    .stdout()
+    .command(['env:var:unset', 'foo', '--environment', 'my-environment2'])
+    .catch((error) => {
+      expect(error.message).to.contain("Couldn't find that app <my-environment2>");
+    })
+    .it('errors with incorrect compute environment');
 });
