bced07dbee0d350103702b2bdefaa905fa67b850
Zane Whitfield <zwhitfield@salesforce.com>
2022-05-20 06:35:11 -0700

    feat: Add json flag to sf env logdrain add (#318)
    * fix: add json functionality and tests
    
    * fix: update logdrain add command and tests
    
    * fix: add json functionality and tests
    
    * fix: update logdrain add command and tests
    
    * fix: remove extra return
    
    * chore: update snapshot
    
    Co-authored-by: Sara D. Cope <scope@salesforce.com>

--- test/commands/env/logdrain/add.test.ts
+++ test/commands/env/logdrain/add.test.ts
@@ -4,6 +4,7 @@
  * Licensed under the BSD 3-Clause license.
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
+import { CLIError } from '@oclif/core/lib/errors';
 import { expect, test } from '@oclif/test';
 import vacuum from '../../../helpers/vacuum';
 
@@ -12,6 +13,18 @@ const APP_NAME = 'my-app';
 const LOG_DRAIN = {
   id: '1',
   url: 'https://logs-r-us.com/1',
+};
+
+const LOG_DRAIN_ENV_ERR = {
+  message: "Couldn't find that app.",
+};
+
+const LOG_DRAIN_INVALID_URL = {
+  message: 'Url is invalid.',
+};
+
+const LOG_DRAIN_ALREADY_USED = {
+  message: 'Url has already been taken',
 };
 
 describe('sf env logdrain add', () => {
@@ -46,6 +59,64 @@ describe('sf env logdrain add', () => {
     .it('will use url if passed using the old flag (not --drain-url)', (ctx) => {
       expect(vacuum(ctx.stderr).replace(/\n[›»]/gm, '')).to.contain(
         vacuum('--url is deprecated and will be removed in a future release. Please use --drain-url going forward.')
+      );
+    });
+
+  test
+    .stdout()
+    .nock('https://api.heroku.com', (api) => api.post(`/apps/${APP_NAME}/log-drains`).reply(200, LOG_DRAIN))
+    .command(['env:logdrain:add', '--target-compute', APP_NAME, '-u', LOG_DRAIN.url, '--json'])
+    .retries(3)
+    .it('will show json output', (ctx) => {
+      expect(vacuum(ctx.stdout).replace(/\n[›»]/gm, '')).to.contain(
+        vacuum(
+          '{\n"status": 0,\n"result": [\n{\n"addon": null,\n"id": "1",\n"url": "https://logs-r-us.com/1"\n}\n],\n"warnings": []\n}'
+        )
+      );
+    });
+
+  test
+    .stdout()
+    .nock('https://api.heroku.com', (api) =>
+      api.post('/apps/invalid-environment/log-drains').reply(404, LOG_DRAIN_ENV_ERR)
+    )
+    .command(['env:logdrain:add', '--target-compute', 'invalid-environment', '-u', LOG_DRAIN.url, '--json'])
+    .catch((error) => {
+      expect((error as CLIError).oclif.exit).to.equal(1);
+    })
+    .it('will show json output error with incorrect compute environment', (ctx) => {
+      expect(vacuum(ctx.stdout).replace(/\n[›»]/gm, '')).to.contain(
+        vacuum('{\n"status": 1,\n"message": "Could not find environment <invalid-environment>"')
+      );
+    });
+
+  test
+    .stdout()
+    .nock('https://api.heroku.com', (api) => api.post(`/apps/${APP_NAME}/log-drains`).reply(422, LOG_DRAIN_INVALID_URL))
+    .command(['env:logdrain:add', '--target-compute', APP_NAME, '-u', 'invalid-url', '--json'])
+    .catch((error) => {
+      expect((error as CLIError).oclif.exit).to.equal(1);
+    })
+    .it('will show json output error with incorrect drain-url', (ctx) => {
+      expect(vacuum(ctx.stdout).replace(/\n[›»]/gm, '')).to.contain(
+        vacuum('{\n"status": 1,\n"message": "URL is invalid <invalid-url>",\n"name": "Error"')
+      );
+    });
+
+  test
+    .stdout()
+    .nock('https://api.heroku.com', (api) =>
+      api.post(`/apps/${APP_NAME}/log-drains`).reply(422, LOG_DRAIN_ALREADY_USED)
+    )
+    .command(['env:logdrain:add', '--target-compute', APP_NAME, '-u', LOG_DRAIN.url, '--json'])
+    .catch((error) => {
+      expect((error as CLIError).oclif.exit).to.equal(1);
+    })
+    .it('will show json output error with drain-url already used', (ctx) => {
+      expect(vacuum(ctx.stdout).replace(/\n[›»]/gm, '')).to.contain(
+        vacuum(
+          '{\n"status": 1,\n"message": "Logdrain URL is already added <https://logs-r-us.com/1>",\n"name": "Error",'
+        )
       );
     });
 });
