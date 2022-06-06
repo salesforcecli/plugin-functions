dbd4225bf0dd90cb2ae8cdf12ba4d785e4d602f2
Sara D. Cope <scope@salesforce.com>
2022-06-06 14:12:48 -0400

    test: add test

--- test/commands/logout/functions.test.ts
+++ test/commands/logout/functions.test.ts
@@ -4,10 +4,11 @@
  * Licensed under the BSD 3-Clause license.
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
-import { test } from '@oclif/test';
+import { expect, test } from '@oclif/test';
 import type { GlobalInfo, SfTokens } from '@salesforce/core';
 import * as sinon from 'sinon';
 import { AuthStubs } from '../../helpers/auth';
+import vacuum from '../../helpers/vacuum';
 
 describe('sf logout functions', () => {
   let contents: SfTokens;
@@ -25,5 +26,14 @@ describe('sf logout functions', () => {
     .command(['logout:functions'])
     .it('removes the functions key from the tokens field on logout', (ctx) => {
       sinon.assert.match(contents, {});
+    });
+  test
+    .stdout()
+    .stderr()
+    .command(['logout:functions', '-j'])
+    .it('will show json output', (ctx) => {
+      expect(vacuum(ctx.stdout).replace(/\n[›»]/gm, '')).to.contain(
+        vacuum('{\n"status": 0,\n"result": [],\n"warnings": []\n}')
+      );
     });
 });
