baa84dd52e4a1847f9c62ca9c512a08fc8bbba74
Sara D. Cope <scope@salesforce.com>
2023-02-15 11:25:04 -0500

    fix: invalidate session on logout

--- src/commands/logout/functions.ts
+++ src/commands/logout/functions.ts
@@ -5,9 +5,16 @@
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
 import { cli } from 'cli-ux';
-import { Messages } from '@salesforce/core';
-
+import { Messages, StateAggregator } from '@salesforce/core';
 import Command from '../../lib/base';
+import APIClient, { herokuClientApiUrl } from '../../lib/api-client';
+
+interface Session {
+  createdAt: string;
+  description: string;
+  id: string;
+  updatedAt: string;
+}
 
 Messages.importMessagesDirectory(__dirname);
 const messages = Messages.loadMessages('@salesforce/plugin-functions', 'logout.functions');
@@ -21,15 +28,46 @@ export default class Login extends Command {
 
   async run() {
     const { flags } = await this.parse(Login);
+
+    const stateAggregator = await StateAggregator.getInstance();
+    const token = stateAggregator.tokens.get('functions-bearer', true)?.token;
+
     this.postParseHook(flags);
 
-    cli.action.start(messages.getMessage('action.start'));
+    cli.action.start(messages.getMessage('action.start'), '...', { stdout: true });
 
-    this.stateAggregator.tokens.unset(Command.TOKEN_BEARER_KEY);
-    await this.stateAggregator.tokens.write();
+    if (!token) {
+      throw new Error('Not authenticated.');
+    }
 
-    cli.action.stop();
+    const client = new APIClient({
+      auth: token,
+      apiUrl: herokuClientApiUrl(),
+    });
 
-    return 'Logged out';
+    try {
+      // get the sesion id
+      const { data: session } = await client.get<Session[]>('/oauth/sessions');
+      const sessionID = session[0].id;
+
+      // delete the session
+      await client.delete<Session>(`/oauth/sessions/${sessionID}`);
+
+      // remove the token from state
+      this.stateAggregator.tokens.unset(Command.TOKEN_BEARER_KEY);
+      await this.stateAggregator.tokens.write();
+
+      cli.action.stop();
+      return 'Logged out';
+    } catch (err: any) {
+      const error = err as Error;
+      if (error.message?.includes('404')) {
+        this.error(new Error('Session not found'));
+      }
+      if (error.message?.includes('401')) {
+        this.error(new Error('Invalid credentials'));
+      }
+      this.error(new Error(error.message));
+    }
   }
 }
